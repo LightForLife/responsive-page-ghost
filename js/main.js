5ba4698fadb9a190ee125a34dddfdf4126d26dee
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-06-03 17:09:30 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -156,3 +156,14 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+/* =============== SCROLL REVEAL ANIMATION ============== */
+
+const sr = ScrollReveal({
+  distance: "90px",
+  duration: 3000,
+});
+
+sr.reveal(".home__data", { origin: "top", delay: 400 });
+sr.reveal(".home__img", { origin: "bottom", delay: 600 });
+sr.reveal(".home__footer", { origin: "top", delay: 800 });
